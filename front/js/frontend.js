4e3e85c62a2b21ce52ab0f471de8021e4613c445
Rodrigo Bossini <professorbossini@gmail.com>
2022-11-03 09:09:49 -0300

    apostila01(html_js_mongodb): codigo 2.13.2 implementado

--- front/js/frontend.js
+++ front/js/frontend.js
@@ -18,3 +18,36 @@ async function obterFilmes(){
     celulaSinopse.innerHTML = filme.sinopse
   }
 }
+
+async function cadastrarFilme(){
+  //1. Construir a URL
+  const URLCompleta = `${protocolo}://${baseURL}${filmesEndpoint}`
+  //2. pegar os inputs em que o usuário digita título e sinopse
+  let tituloInput = document.querySelector('#tituloInput')
+  let sinopseInput = document.querySelector('#sinopseInput')
+  //3. pegar os valores do filme (titulo e sinopse)
+  let titulo = tituloInput.value
+  let sinopse = sinopseInput.value
+  let filme = {titulo, sinopse}
+  //4. limpar os campos em que o usuário digitou
+  tituloInput.value = ''
+  sinopseInput.value = ''
+  //5. Enviar o filme ao back end
+  const filmes = (await axios.post(URLCompleta, filme)).data
+  //6. remover os filmes todos da DOM, para que eu possa inserir a coleção recebida do back
+  const tabela = document.querySelector('.filmes')
+  const corpoTabela = tabela.getElementsByTagName('tbody')[0]
+  corpoTabela.innerHTML = ''
+  //iterar sobre a coleção de filmes
+  console.log(filmes)
+  for(let filme of filmes){
+    //adicionar uma linha na árvore
+    let linha = corpoTabela.insertRow(0)
+    //na linha, adicionar duas células
+    let celulaTitulo = linha.insertCell(0)
+    let celulaSinopse = linha.insertCell(1)
+    //em cada célula, colocar titulo e sinopse
+    celulaTitulo.innerHTML = filme.titulo
+    celulaSinopse.innerHTML = filme.sinopse    
+  }
+}
